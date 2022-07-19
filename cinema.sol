23b2927e34708d121cb89e72e2052a0d3441fb3b
KishoreVB70 <kishore07kzr@gmail.com>
2022-07-19 07:07:39 +0530

    made changes and added functionality

--- cinema.sol
+++ cinema.sol
@@ -1,87 +1,79 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.8.11;
+pragma solidity ^0.8.5;
 
 contract Cinema{
     //owner of the cinema
     address payable public owner;
 
-    uint8 public totalSeat = 3;
+    uint8 public totalSeats;
 
-    // The theatre could have seats that are free or they could all be used
-    // so we will create an Enum for the condition of seats availability in the theatre
-    // it will contain two variables 
-    // 1. free
-    // 2. finished
+    string public movieName;
 
-    enum SeatAvailability{Free, Finished}
-    SeatAvailability public seatAvailability;
+    uint256 public seatPrice;
+
+    uint256 public bookingclosingTime;
+
+    uint256 public seatsAvailable;
+
 
     // Events to log data of transaction incase someone wants to subscribe to it
-    event BookedSeats(address _buyer, uint _amountPaid);
+    event BookedSeats(address _buyer, uint _seats, uint _price);
+    event movieChanged(string _movieName, uint _seats, uint _price);
 
-    constructor(){
+    constructor(uint8 _seats, string memory _movieName, uint _price, uint _hoursTillBookingFinishes){
         // Saves the creator address as the owner when the smart contract is created 
         owner = payable(msg.sender);
-        // Initially sets the seats to free when the contract is created
-        seatAvailability = SeatAvailability.Free;
+        totalSeats = _seats;
+        seatsAvailable = _seats;
+        movieName = _movieName;
+        seatPrice = _price;
+        //In hours
+        bookingclosingTime = _hoursTillBookingFinishes * 3600;
     }
 
-    // Reduce the total number of seat number internally once a seat is bought
-    function reduceSeatNumber()internal{
-        totalSeat = totalSeat - 1;
-    }
-
-    // set the availability of seat according to the amount of seats remaining
-    function setAvailability() internal {
-        if(totalSeat > 0){
-            seatAvailability = SeatAvailability.Free;
-        }
-        else{
-            seatAvailability = SeatAvailability.Finished;
-        }
-    }
-
-    //modifier function to allow purchase only when the availability condition is set to Free
-    modifier allowPurchaseWhenFree {
-        require(seatAvailability == SeatAvailability.Free, "There are no seats available for this movie please check back tomorrow");
+    modifier onlyOwner{
+        require(msg.sender == owner, "Only the owner can access this function");
         _;
     }
-
-    modifier ensureUserHasEnoughMoney (uint _amount) {
-        require(msg.value > _amount, "You need 6 ether to book  the seat, 5 ethers for the seat with little change for gas fee, so approximately 6 ether for the booking");
-        _;
-    }
-
 
 
     // Function to book a seat for the box office movie avengers the end game
-    function bookSeatForAvengersEndgame() public payable allowPurchaseWhenFree ensureUserHasEnoughMoney( 5 ether) {
+    function bookSeat(uint _numberOfSeats) public payable {
+        require(seatsAvailable > _numberOfSeats, "Seats not available");
+        require(msg.value == _numberOfSeats * seatPrice, "Send correct amount");
         
         owner.transfer(msg.value);
 
-        reduceSeatNumber();
+        reduceSeatNumber(_numberOfSeats);
 
-        setAvailability();
-
-        emit BookedSeats(msg.sender, msg.value);
+        emit BookedSeats(msg.sender, _numberOfSeats, seatPrice);
     }
 
-    //Owner modifier function to require that only creator of the smart contract can reset the cinema seat to the original value
-    modifier ownersCheck {
-        require( payable(msg.sender) == payable(owner), "You cannot reset the seat, you do not have access priviledge to perform this action");
-        _;
-    }
-
-
-    //Reset all the seats to empty after the movie
+    //Reset movie details to empty after the movie
     //This function can only be carried out by the owner of the smart contract
 
-    function resetTotalSeatsAvailable() public ownersCheck returns(uint8){
-        totalSeat = 3;
-        return totalSeat;
+    function setForNextShow(string memory _movieName, uint _price, uint8 _seats, uint _hoursTillBookingFinishes ) public onlyOwner{
+        require(msg.sender == owner, "Only the owner can access this function");
+        require(block.timestamp > bookingclosingTime, "Movie booking not over");
+        totalSeats = _seats;
+        seatsAvailable = _seats;
+        movieName = _movieName;
+        seatPrice = _price;
+        bookingclosingTime = _hoursTillBookingFinishes * 3600;
+
+        emit movieChanged(_movieName, _seats, _price);
     }
 
+    //Incase of an offline booking, the owner can block the seats
+    function blockSeats(uint _seats) public onlyOwner{
+        reduceSeatNumber(_seats);
+    }
+
+    // Reduce the total number of seat number internally once a seat is bought
+    function reduceSeatNumber(uint _numberOfSeats) internal{
+        seatsAvailable = seatsAvailable - _numberOfSeats;
+    }
 
     
 }
